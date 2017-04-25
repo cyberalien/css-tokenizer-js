a64d98d7ceeb2fa58915cf0038200a9198ff72cd
cyberalien <cyberalien@gmail.com>
2017-04-25 16:46:31 +0300

    Remove indexes from test results, add test for less mixins with variables

--- tests/tokenizer/less_test.js
+++ tests/tokenizer/less_test.js
@@ -19,19 +19,19 @@
             });
             result = parser.tokenize('.box-shadow() { color: red; }');
             expect(parser.errors.length).to.be.equal(0);
+            result.forEach(item => {
+                delete item.index;
+            });
             result.should.be.eql([{
                 token: '{',
                 code: '.box-shadow()',
-                selectors: ['.box-shadow()'],
-                index: 0
+                selectors: ['.box-shadow()']
             }, {
                 token: 'rule',
                 key: 'color',
-                value: 'red',
-                index: 15
+                value: 'red'
             }, {
-                token: '}',
-                index: 28
+                token: '}'
             }]);
         });
 
@@ -44,29 +44,27 @@
             });
             result = parser.tokenize('$test: black; $foo: lighten(#400, 20%); a { color: $test; }');
             expect(parser.errors.length).to.be.equal(0);
+            result.forEach(item => {
+                delete item.index;
+            });
             result.should.be.eql([{
                 token: 'rule',
                 key: '$test',
-                value: 'black',
-                index: 0
+                value: 'black'
             }, {
                 token: 'rule',
                 key: '$foo',
-                value: 'lighten(#400, 20%)',
-                index: 13
+                value: 'lighten(#400, 20%)'
             }, {
                 token: '{',
                 code: 'a',
-                selectors: ['a'],
-                index: 39
+                selectors: ['a']
             }, {
                 token: 'rule',
                 key: 'color',
-                value: '$test',
-                index: 43
+                value: '$test'
             }, {
-                token: '}',
-                index: 58
+                token: '}'
             }]);
 
             // Mixin with variables
@@ -86,54 +84,122 @@
   .mixin(lighten(#efca44, 10%); @padding: 40px);
 }`);
             expect(parser.errors.length).to.be.equal(0);
+            result.forEach(item => {
+                delete item.index;
+            });
+
             result.should.be.eql([{
                 token: '{',
                 code: '.mixin(@color: black; @margin: 10px; @padding: 20px)',
-                selectors: ['.mixin(@color: black; @margin: 10px; @padding: 20px)'],
-                index: 0
+                selectors: ['.mixin(@color: black; @margin: 10px; @padding: 20px)']
             }, {
                 token: 'rule',
                 key: 'color',
-                value: '@color',
-                index: 54
+                value: '@color'
             }, {
                 token: 'rule',
                 key: 'margin',
-                value: '@margin',
-                index: 71
+                value: '@margin'
             }, {
                 token: 'rule',
                 key: 'padding',
-                value: '@padding',
-                index: 90
+                value: '@padding'
             }, {
-                token: '}',
-                index: 112
+                token: '}'
             }, {
                 token: '{',
                 code: '.class1',
-                selectors: ['.class1'],
-                index: 113
+                selectors: ['.class1']
             }, {
                 token: 'code',
-                code: '.mixin(@margin: 20px; @color: #33acfe);',
-                index: 123
+                code: '.mixin(@margin: 20px; @color: #33acfe);'
             }, {
-                token: '}',
-                index: 166
+                token: '}'
             }, {
                 token: '{',
                 code: '.class2',
-                selectors: ['.class2'],
-                index: 167
+                selectors: ['.class2']
             }, {
                 token: 'code',
-                code: '.mixin(lighten(#efca44, 10%); @padding: 40px);',
-                index: 177
+                code: '.mixin(lighten(#efca44, 10%); @padding: 40px);'
             }, {
-                token: '}',
-                index: 227
+                token: '}'
             }]);
         });
+
+        it('mixin with variables', () => {
+            let parser, result;
+
+            parser = new Tokenizer({
+                ignoreErrors: false,
+                lessSyntax: true
+            });
+            result = parser.tokenize(`.box-shadow(@style, @c) when (iscolor(@c)) {
+  -webkit-box-shadow: @style @c;
+  box-shadow:         @style @c;
+}
+.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha)) {
+  .box-shadow(@style, rgba(0, 0, 0, @alpha));
+}
+.box {
+  color: saturate(@base, 5%);
+  border-color: lighten(@base, 30%);
+  div { .box-shadow(0 0 5px, 30%) }
+}
+`);
+            expect(parser.errors.length).to.be.equal(0);
+            result.forEach(item => {
+                delete item.index;
+            });
+
+            result.should.be.eql([{
+                token: '{',
+                code: '.box-shadow(@style, @c) when (iscolor(@c))',
+                selectors: ['.box-shadow(@style, @c) when (iscolor(@c))']
+            }, {
+                token: 'rule',
+                key: '-webkit-box-shadow',
+                value: '@style @c'
+            }, {
+                token: 'rule',
+                key: 'box-shadow',
+                value: '@style @c'
+            }, {
+                token: '}'
+            }, {
+                token: '{',
+                code: '.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha))',
+                selectors: ['.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha))']
+            }, {
+                token: 'code',
+                code: '.box-shadow(@style, rgba(0, 0, 0, @alpha));'
+            }, {
+                token: '}'
+            }, {
+                token: '{',
+                code: '.box',
+                selectors: ['.box']
+            }, {
+                token: 'rule',
+                key: 'color',
+                value: 'saturate(@base, 5%)'
+            }, {
+                token: 'rule',
+                key: 'border-color',
+                value: 'lighten(@base, 30%)'
+            }, {
+                token: '{',
+                code: 'div',
+                selectors: ['div']
+            }, {
+                token: 'code',
+                code: '.box-shadow(0 0 5px, 30%)'
+            }, {
+                token: '}'
+            }, {
+                token: '}'
+            }]);
+        });
+
     });
 })();
